c6ea3690e84f7c4cdc107baa8d65a7be61abb5fc
Victor Manuel Gonzalez Opazo <Victor.Opazo@tsoftglobal.com>
2019-11-11 10:07:00 -0300

    Change port

--- app.py
+++ app.py
@@ -23,4 +23,4 @@ def download(filename):
     return send_from_directory(directory='output', filename=filename,as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=8080)
